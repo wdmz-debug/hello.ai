b7131fa37da167f1bb55dbcb0afcbd97e13c53f5
wdmz-debug <2101095592@qq.com>
2026-04-22 11:10:52 +0800

    typical C++ memory leak example

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
-#include <vector>
-int main() {
-    std::vector<int> nums = {10, 20, 30, 40, 50};
-    for(int &x : nums){
-        //打印值：x
-        //打印地址：&x
-        std::cout << "值：" << x << "地址：" << &x << std::endl;
-    }
+class AIModel {
+    public:
+    //构造函数：创建时自动调用
+    AIModel() { std::cout << "AI模型已加载到内存中占用(500MB)...\n";}
+    //析构函数：销毁时自动调用
+    ~AIModel() { std::cout << "AI模型已从内存中完全清理\n";}
+    void run() { std::cout << "正在执行目标检测推理...\n";}
+};
+
+void processImage() {
+    std::cout << "开始处理单张照片\n";
+    //案发现场：在堆上分配内存
+    AIModel* model = new AIModel();
+    model->run();
+    std::cout << "图片处理结束";
+}
+
+int main(){
+    std::cout << "部署系统启动";
+    processImage();
+    std::cout << "部署系统关闭";
+    return 0;
 }
